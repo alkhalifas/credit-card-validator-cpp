0d8aab6fa2d7cc369b4d4b3730b19ee97c7b80a7
Saleh Alkhalifa <salehesam@gmail.com>
2021-08-09 22:34:04 -0400

    working commit

---
+++ cmake-build-debug/CMakeFiles/credit_card_validator_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/credit_card_validator_cpp.dir/main.cpp.obj"
+  "credit_card_validator_cpp.exe"
+  "credit_card_validator_cpp.exe.manifest"
+  "credit_card_validator_cpp.pdb"
+  "libcredit_card_validator_cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/credit_card_validator_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
